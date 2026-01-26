ed910972a6842440c62cc25de3db5b7af11303d8
ex <113571950+securitydied@users.noreply.github.com>
2026-01-26 14:46:18 -0300

    Update diparis.js

--- diparis.js
+++ diparis.js
@@ -1,17 +1,25 @@
+document.body.innerHTML = "";
+document.body.style.margin = "0";
+
 const box = document.createElement("div");
 box.innerHTML = `
-<center>
   <h1>hacked by diparis</h1>
-  <p>Greetz: jot1nh4, r0dr1g0, luc4s, k4w4n, ry4n, m4rc3l, jun1n.</p>
-  </center>
+  <p>jotinh4, r0dr1g0, luc4s, k4w4n, ry4n, m4rc3l, jun1n</p>
 `;
 
 box.style.cssText = `
+  position: fixed;
+  inset: 0;
   background: black;
   color: white;
-  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   text-align: center;
-  font-family: Arial;
+  font-family: Arial, Helvetica, sans-serif;
 `;
 
 document.body.appendChild(box);
