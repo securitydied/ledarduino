2c3360718383ff483b650d3931ac30b5f7d6cf35
ex <113571950+securitydied@users.noreply.github.com>
2026-01-26 14:44:25 -0300

    Update diparis.js

--- diparis.js
+++ diparis.js
@@ -1,1 +1,17 @@
-hacked by diparis
+const box = document.createElement("div");
+box.innerHTML = `
+<center>
+  <h1>hacked by diparis</h1>
+  <p>Greetz: jot1nh4, r0dr1g0, luc4s, k4w4n, ry4n, m4rc3l, jun1n.</p>
+  </center>
+`;
+
+box.style.cssText = `
+  background: black;
+  color: white;
+  padding: 40px;
+  text-align: center;
+  font-family: Arial;
+`;
+
+document.body.appendChild(box);
